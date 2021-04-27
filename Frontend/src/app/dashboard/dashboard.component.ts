355e458929071a99332407c0c13398f03da18a1e
HelderMCarvalho <a15310@alunos.ipca.pt>
2021-04-27 11:33:44 +0100

    FIX code;

--- Frontend/src/app/dashboard/dashboard.component.ts
+++ Frontend/src/app/dashboard/dashboard.component.ts
@@ -87,7 +87,7 @@ export class DashboardComponent implements OnInit {
             chartPadding: {top: 0, right: 0, bottom: 0, left: 0},
         }
 
-        var dailySalesChart = new Chartist.Line('#dailySalesChart', dataDailySalesChart, optionsDailySalesChart);
+        const dailySalesChart = new Chartist.Line('#dailySalesChart', dataDailySalesChart, optionsDailySalesChart);
 
         this.startAnimationForLineChart(dailySalesChart);
 
@@ -110,7 +110,7 @@ export class DashboardComponent implements OnInit {
             chartPadding: {top: 0, right: 0, bottom: 0, left: 0}
         }
 
-        var completedTasksChart = new Chartist.Line('#completedTasksChart', dataCompletedTasksChart, optionsCompletedTasksChart);
+        const completedTasksChart = new Chartist.Line('#completedTasksChart', dataCompletedTasksChart, optionsCompletedTasksChart);
 
         // start animation for the Completed Tasks Chart - Line Chart
         this.startAnimationForLineChart(completedTasksChart);
@@ -118,14 +118,14 @@ export class DashboardComponent implements OnInit {
 
         /* ----------==========     Emails Subscription Chart initialization    ==========---------- */
 
-        var datawebsiteViewsChart = {
+        const datawebsiteViewsChart = {
             labels: ['J', 'F', 'M', 'A', 'M', 'J', 'J', 'A', 'S', 'O', 'N', 'D'],
             series: [
                 [542, 443, 320, 780, 553, 453, 326, 434, 568, 610, 756, 895]
 
             ]
         };
-        var optionswebsiteViewsChart = {
+        const optionswebsiteViewsChart = {
             axisX: {
                 showGrid: false
             },
@@ -133,7 +133,7 @@ export class DashboardComponent implements OnInit {
             high: 1000,
             chartPadding: {top: 0, right: 5, bottom: 0, left: 0}
         };
-        var responsiveOptions: any[] = [
+        const responsiveOptions: any[] = [
             ['screen and (max-width: 640px)', {
                 seriesBarDistance: 5,
                 axisX: {
@@ -143,9 +143,10 @@ export class DashboardComponent implements OnInit {
                 }
             }]
         ];
-        var websiteViewsChart = new Chartist.Bar('#websiteViewsChart', datawebsiteViewsChart, optionswebsiteViewsChart, responsiveOptions);
+        const websiteViewsChart = new Chartist.Bar('#websiteViewsChart', datawebsiteViewsChart, optionswebsiteViewsChart,
+            responsiveOptions);
 
-        //start animation for the Emails Subscription Chart
+        // start animation for the Emails Subscription Chart
         this.startAnimationForBarChart(websiteViewsChart);
     }
 
--- Frontend/src/app/layouts/admin-layout/admin-layout.module.ts
+++ Frontend/src/app/layouts/admin-layout/admin-layout.module.ts
@@ -8,7 +8,6 @@ import {UserProfileComponent} from '../../user-profile/user-profile.component';
 import {TableListComponent} from '../../table-list/table-list.component';
 import {TypographyComponent} from '../../typography/typography.component';
 import {IconsComponent} from '../../icons/icons.component';
-import {MapsComponent} from '../../maps/maps.component';
 import {NotificationsComponent} from '../../notifications/notifications.component';
 import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
@@ -36,7 +35,6 @@ import {MatSelectModule} from '@angular/material/select';
         TableListComponent,
         TypographyComponent,
         IconsComponent,
-        MapsComponent,
         NotificationsComponent,
     ]
 })
